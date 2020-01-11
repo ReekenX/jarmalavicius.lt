331c1ec4063a7f1f3bb743caeae6b2ab93bc8ee7
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2020-01-11 08:10:18 +0200

    Drop mobile menu Vue component

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import CollapseMenu from './components/CollapseMenu'
-import MobileMenu from './components/MobileMenu'
 
 Vue.component('collapseMenu', CollapseMenu)
-Vue.component('mobileMenu', MobileMenu)
 
 const app = new Vue({
   el: '#app',
